36fa5f70aebe452f51f560c97828285382a786d7
grey <109694353+gray1717@users.noreply.github.com>
2024-07-01 21:40:25 +0300

    Update ArchInstall.sh

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -1,27 +1,94 @@
-#!/bin/sh
+#!/bin/zsh
+
+select_disk()
+{
+    disks=()
+    for device in $(lsblk -prndo NAME); do
+        disk_size=$(lsblk -prndo SIZE $device)
+        disks+=($device $disk_size)
+    done
+    local disk
+    local result
+    disk=$(dialog --title "Disc selection" --stdout --menu "Select a disk from the list to install ArchLinux" 16 32  1 ${disks[@]})
+    result=$?
+    echo $disk
+    return $result
+}
+
+set_user_pswd()
+{
+    local pswd
+    local result
+    local warning
+    locale confirm_pswd
+    while :; do
+        pswd=$(dialog --title "Setting a password for the user "$1 --stdout --insecure --passwordbox "${warning}enter password" 16 48)
+        result=$?
+        [ $result -eq 1 ] && exit $result
+        [ ! -z $pswd ] && break
+        warning="Empty Password!\n"
+    done
+    warning=""
+    while :; do
+        confirm_pswd=$(dialog --title "Setting a password for the user "$1 --stdout --insecure --passwordbox "${warning}Confirm the password" 16 48)
+        result=$?
+        [ $result -eq 1 ] && exit $result
+        [ $confirm_pswd = $pswd ] && break
+        warning="Passwords Do No Match!\n"
+    done
+    echo $pswd
+    return $result
+}
+
+set_user_name()
+{
+    local user_name
+    local result
+    local warning
+    while :; do
+        user_name=$(dialog --title "Creating a user" --stdout --inputbox "${warning}Enter your new username" 16 48)
+        result=$?
+        [ $result -eq 1 ] && exit $result
+        [[ $user_name =~ ^[A-Za-z]\W* ]] && break
+        warning="Empty or incorrect username!\n"
+        #break
+    done
+    echo $user_name
+    return $result
+}
 
 clear
+pacman -Sy dialog --noconfirm
+if [ $? -ne 0 ]; then
+    echo -e "package \"dialog\" is not installed"; exit
+fi
+[ -n "$(mount | grep /mnt)" ] && umount -R /mnt
+dialog --title ArchInstall --ok-label "go" --msgbox \
+"This is the Arch Linux installation script.\nNext, enter all the data necessary for installation.\nThe Cancel button aborts the script" 16 48
 
-disk="/dev/sda"
-root_pswd="0000"
-user_name="user"
-user_pswd="1111"
+disk=$(select_disk); [ $? -eq 1 ] && exit
+root_pswd=$(set_user_pswd "root"); [ $? -eq 1 ] && exit
+user_name=$(set_user_name); [ $? -eq 1 ] && exit
+user_pswd=$(set_user_pswd "$user_name"); [ $? -eq 1 ] && exit
 
-wipefs -a $disk
-parted $disk mklabel gpt mkpart EFI 1049kB 106MB set 1 boot on mkpart ArchLinux 106MB 100%
-mkfs.vfat -n EFI $disk"1"
-mkfs.btrfs -f -L ArchLinux $disk"2"
+clear
+disk_s=${disk//\//\\\/}
+part_efi=$(lsblk -prno NAME | sed -n "/$disk_s\w\w*/p" | sed -n 1p)
+part_root=$(lsblk -prno NAME | sed -n "/$disk_s\w\w*/p" | sed -n 2p)
+echo -e "g\nn\n\n\n+300M\nn\n\n\n\nw" | fdisk -w always -W always $disk
+mkfs.vfat -n EFI $part_efi
+mkfs.btrfs -f -L ArchLinux $part_root
 
-mount $disk"2" /mnt
+mount $part_root /mnt
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
 umount /mnt
 
-mount -o noatime,compress=zstd:3,discard=async,subvol=@ $disk"2" /mnt
-mount --mkdir -o noatime $disk"1" /mnt/boot/efi
-mount --mkdir -o noatime,compress=zstd:3,discard,subvol=@home $disk"2" /mnt/home
+mount -o noatime,compress=zstd:3,discard,subvol=@ $part_root /mnt
+mount --mkdir -o noatime $part_efi /mnt/boot/efi
+mount --mkdir -o noatime,compress=zstd:3,discard,subvol=@home $part_root /mnt/home
 
-pacstrap /mnt base linux linux-firmware btrfs-progs intel-ucode nano grub networkmanager sudo htop neofetch
+pacstrap /mnt base linux linux-firmware btrfs-progs intel-ucode grub networkmanager sudo nano htop neofetch
 #efibootmgr
 
 genfstab -U /mnt | sed "s/subvolid=[[:digit:]]\+,//" > /mnt/etc/fstab
@@ -44,5 +111,5 @@ arch-chroot /mnt locale-gen
 echo "LANG=ru_RU.UTF-8" > /mnt/etc/locale.conf
 echo "FONT=cyr-sun16" > /mnt/etc/vconsole.conf
 
-echo -e "\n\n=======Installation completed!======="
-
+umount -R /mnt
+dialog --infobox 'Installation completed!' 4 32
